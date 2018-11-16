7ca6200c133d4f0c1427088c009abe8aeafdc344
Rok Kos <rocki5555@gmail.com>
2018-11-16 07:49:42 +0100

    01-premikanje: Demo prezentacija

---
+++ 01-premikanje/OsnovnoPremikanje/Assets/Scripts/IgralecController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgralecController : MonoBehaviour
+{
+    [SerializeField] float hitrost = 1.5f;
+    [SerializeField] Vector3 zacetna_pozicija = new Vector3(0, -2, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Premikanje.NastaviPozicijo(gameObject, zacetna_pozicija);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            Premikanje.PremikGor(gameObject, hitrost * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            Premikanje.PremikDol(gameObject, hitrost * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            Premikanje.PremikLevo(gameObject, hitrost * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            Premikanje.PremikDesno(gameObject, hitrost * Time.deltaTime);
+        }
+    }
+}
---
+++ 01-premikanje/OsnovnoPremikanje/Assets/Scripts/Premikanje.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Premikanje : MonoBehaviour
+{
+    public static void PremikGor(GameObject telo, float premik) {
+        Vector3 pozicija = telo.transform.position;
+        pozicija.y += premik;
+        telo.transform.position = pozicija;
+    }
+
+    public static void PremikDol(GameObject telo, float premik) {
+        Vector3 pozicija = telo.transform.position;
+        pozicija.y -= premik;
+        telo.transform.position = pozicija;
+    }
+
+    public static void PremikLevo(GameObject telo, float premik) {
+        Vector3 pozicija = telo.transform.position;
+        pozicija.x -= premik;
+        telo.transform.position = pozicija;
+    }
+
+    public static void PremikDesno(GameObject telo, float premik) {
+        Vector3 pozicija = telo.transform.position;
+        pozicija.x += premik;
+        telo.transform.position = pozicija;
+    }
+
+    public static void PremikNaprej(GameObject telo, float premik) {
+        Vector3 pozicija = telo.transform.position;
+        pozicija.z += premik;
+        telo.transform.position = pozicija;
+    }
+
+    public static void PremikNazaj(GameObject telo, float premik) {
+        Vector3 pozicija = telo.transform.position;
+        pozicija.z -= premik;
+        telo.transform.position = pozicija;
+    }
+
+    public static void NastaviPozicijo(GameObject telo, Vector3 pozicija) {
+        telo.transform.position = pozicija;
+    }
+}
