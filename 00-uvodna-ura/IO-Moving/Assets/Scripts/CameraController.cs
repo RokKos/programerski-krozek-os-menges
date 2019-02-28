137eda9182cc86524911efad1cd90e34d7de5f58
RokKos <rocki5555@gmail.com>
2019-02-28 19:03:01 +0100

    Some improvments to the moving scene

---
+++ 00-uvodna-ura/IO-Moving/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField] Transform player;
+    [SerializeField] Vector3 offset;
+    [Range(0.0f,1.0f)]
+    [SerializeField] float lerpFactor;
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        Vector3 cameraPos = player.position;
+        cameraPos += offset;
+        cameraPos = Vector3.Lerp(transform.position,cameraPos, lerpFactor);
+
+        transform.position = cameraPos;
+    }
+}
